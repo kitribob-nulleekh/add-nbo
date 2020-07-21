01b9b0807283c96aa01df21bd2b3705710ebec56
nulLeeKH <i_am@nulleekh.com>
2020-07-21 20:19:29 +0900

    [REFACTOR] Refactor main.cpp

--- main.cpp
+++ main.cpp
@@ -20,6 +20,25 @@ int printUsage(char *dir) {
     return 0;
 }
 
+uint32_t getData(char *filePath) {
+	FILE *file;
+	uint32_t num;
+
+    printf("\nINFO: Strat reading data from file | filePath=%s\n", filePath); //debugLog
+    file = fopen(filePath, "rb");
+    printf("INFO: File has been opened | file=%p\n", file); //debugLog
+    fread(&num, 1, sizeof(uint32_t), file);
+    printf("INFO: Data has been readed | num=0x%x\n", num); //debugLog
+    fclose(file);
+    printf("INFO: File has been closed\n\n\n"); //debugLog
+
+    printf("\nINFO: Convert data from host byte order to network byte order | num=0x%x\n", num); //debugLog
+    num = htonl(num);
+    printf("INFO: Data has been converted | num=0x%x\n\n\n", num); //debugLog
+
+    return num;
+}
+
 int main(int argc, char* argv[]) {
     printTitle();
 
@@ -27,33 +46,16 @@ int main(int argc, char* argv[]) {
         printUsage(argv[0]);
     } else {
         printf("\nINFO: Start initialize add-nbo\n"); //debugLog
-        FILE *firstFile, *secondFile;
         uint32_t firstNum, secondNum, result;
         printf("INFO: Initialized successfully\n\n\n"); //debugLog
 
-        printf("\nINFO: Strat reading data from frist file\n"); //debugLog
-        firstFile = fopen(argv[1], "rb");
-        printf("INFO: First file has been opened | firstFile=%p\n", firstFile); //debugLog
-        fread(&firstNum, 1, sizeof(uint32_t), firstFile);
-        printf("INFO: Data has been readed | firstNum=0x%x\n", firstNum); //debugLog
-        fclose(firstFile);
-        printf("INFO: First file has been closed\n\n\n"); //debugLog
+        printf("\nINFO: Get data from first file | argv[1]=%s\n", argv[1]); //debugLog
+        firstNum = getData(argv[1]);
+        printf("INFO: Get data from second file successfully | firstNum=0x%x\n\n\n", firstNum); //debugLog
 
-        printf("\nINFO: Strat reading data from second file\n"); //debugLog
-        secondFile = fopen(argv[2], "rb");
-        printf("INFO: Second file has been opened | secondFile=%p\n", secondFile); //debugLog
-        fread(&secondNum, 1, sizeof(uint32_t), secondFile);
-        printf("INFO: Data has been readed | secondNum=0x%x\n", secondNum); //debugLog
-        fclose(secondFile);
-        printf("INFO: Second file has been closed\n\n\n"); //debugLog
-
-        printf("\nINFO: Convert first data from host byte order to network byte order | firstNum=0x%x\n", firstNum); //debugLog
-        firstNum = htonl(firstNum);
-        printf("INFO: First data has been converted | firstNum=0x%x\n\n\n", firstNum); //debugLog
-
-        printf("\nINFO: Convert second data from host byte order to network byte order | secondNum=0x%x\n", secondNum); //debugLog
-        secondNum = htonl(secondNum);
-        printf("INFO: Second data has been converted | secondNum=0x%x\n\n\n", secondNum); //debugLog
+        printf("\nINFO: Get data from second file | argv[2]=%s\n", argv[2]); //debugLog
+        secondNum = getData(argv[2]);
+        printf("INFO: Get data from second file successfully | secondNum=0x%x\n\n\n", secondNum); //debugLog
 
         printf("\nINFO: Calculating started | firstNum=0x%x | secondNum=0x%x\n", firstNum, secondNum); //debugLog
         result = firstNum + secondNum;
